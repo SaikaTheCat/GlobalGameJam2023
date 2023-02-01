c252b11c6ddee7add750caf47c329de6e25894a3
sejoon-oh <miau.gds@gmail.com>
2023-02-01 16:50:50 -0300

    equisde

--- Assets/RootController.cs
+++ Assets/RootController.cs
@@ -3,44 +3,55 @@ using UnityEngine;
 public class RootController : MonoBehaviour
 {
 	public Sprite[] sprites;
-	public GameObject particleEffect;
+	//public GameObject particleEffect;
+	public Vector2 gridSize = new Vector2(1, 1);
 
 	private int currentSprite = 0;
+	private Vector2 currentGridPos;
 
-	void Update()
+	private void Start()
+	{
+		// Establecer la posición inicial del grid
+		currentGridPos = new Vector2(0, 0);
+	}
+
+	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			currentSprite = 1;
-			SpawnSprite(sprites[currentSprite]);
+			MoveSprite(new Vector2(0, gridSize.y));
 		}
 		else if (Input.GetKeyDown(KeyCode.DownArrow))
 		{
 			currentSprite = 2;
-			SpawnSprite(sprites[currentSprite]);
+			MoveSprite(new Vector2(0, -gridSize.y));
 		}
 		else if (Input.GetKeyDown(KeyCode.RightArrow))
 		{
 			currentSprite = 3;
-			SpawnSprite(sprites[currentSprite]);
+			MoveSprite(new Vector2(gridSize.x, 0));
 		}
 		else if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
 			currentSprite = 4;
-			SpawnSprite(sprites[currentSprite]);
+			MoveSprite(new Vector2(-gridSize.x, 0));
 		}
 	}
 
-	private void SpawnSprite(Sprite sprite)
+	private void MoveSprite(Vector2 direction)
 	{
 		// Instanciar el sprite y el efecto de particula
 		GameObject newSprite = new GameObject();
-		newSprite.AddComponent<SpriteRenderer>().sprite = sprite;
-		newSprite.transform.position = transform.position;
+		newSprite.AddComponent<SpriteRenderer>().sprite = sprites[currentSprite];
+		newSprite.transform.position = (Vector3)currentGridPos + (Vector3)direction;
 
-		GameObject particle = Instantiate(particleEffect, transform.position, Quaternion.identity);
+		//GameObject particle = Instantiate(particleEffect, (Vector3)currentGridPos, Quaternion.identity);
 
-		// Borrar el sprite anterior
-		Destroy(gameObject);
+		// Actualizar la posición actual del grid
+		currentGridPos += direction;
+
+		// Borrar el efecto de particula
+		//Destroy(particle, particle.GetComponent<ParticleSystem>().main.duration);
 	}
 }
