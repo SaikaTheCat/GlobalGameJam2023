b6ace28757d6fedb61cbe18fa3783011f016fbed
Mboitu <kurupy.jmg@gmail.com>
2023-02-04 16:29:38 -0300

    add main menu

---
+++ Assets/Scripts/MainMenuScreen.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private List<Button> selectedLevelsButtons;
+    [SerializeField] private Button exitButton;
+    void Start()
+    {
+        exitButton.onClick.AddListener(() => Application.Quit());
+        int level = 1;
+        selectedLevelsButtons.ForEach(buttom =>
+        {
+            var l = level;
+            buttom.onClick.AddListener(
+                () => SceneManager.LoadScene(l)
+                );
+            level++;
+        });
+    }
+
+
+}
