0f63fe8319e36aa132f33b208f9e446c2f04da66
Mboitu <kurupy.jmg@gmail.com>
2023-02-05 15:08:33 -0300

    win screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 public class Player
 {
     private readonly static Player _instance = new Player();
-
+    public static string nexLevel;
     public static Player Instance => _instance;
 
     public int maxScore;
     public List<int> levels;
+
 }
